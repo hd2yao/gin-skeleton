a57619d7ad3358a2cb90319436d090a6d089d311
hd2yao <1054290355@qq.com>
2023-08-18 18:00:25 +0800

    finish ConfigFileChangeListen and Clone method

--- app/utils/yml_config/yml_config.go
+++ app/utils/yml_config/yml_config.go
@@ -1,14 +1,17 @@
 package yml_config
 
 import (
-	"github.com/hd2yao/gin-skeleton/app/global/my_errors"
-	"github.com/hd2yao/gin-skeleton/app/global/variable"
-	"github.com/hd2yao/gin-skeleton/app/utils/yml_config/ymlconfig_interf"
+	"go.uber.org/zap"
 	"log"
 	"sync"
 	"time"
 
+	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/viper"
+
+	"github.com/hd2yao/gin-skeleton/app/global/my_errors"
+	"github.com/hd2yao/gin-skeleton/app/global/variable"
+	"github.com/hd2yao/gin-skeleton/app/utils/yml_config/ymlconfig_interf"
 )
 
 var lastChangeTime time.Time
@@ -47,13 +50,46 @@ type ymlConfig struct {
 	mu    *sync.Mutex
 }
 
+// ConfigFileChangeListen 监听文件变化
 func (y *ymlConfig) ConfigFileChangeListen() {
+	y.viper.OnConfigChange(func(changeEvent fsnotify.Event) {
+		if time.Now().Sub(lastChangeTime).Seconds() >= 1 {
+			if changeEvent.Op.String() == "WRITE" {
+				y.clearCache()
+				lastChangeTime = time.Now()
+			}
+		}
+	})
+	y.viper.WatchConfig()
+}
+
+// 判断项关键是否已经缓存
+func (y *ymlConfig) keyIsCache(keyName string) bool {
+	return false
+}
+
+// 对键值进行缓存
+func (y *ymlConfig) cache(keyName string, value interface{}) bool {
+	return false
+}
+
+// 清空已经缓存的配置项信息
+func (y *ymlConfig) clearCache() {
 
 }
 
+// Clone 允许 clone 一个相同功能的结构体
 func (y *ymlConfig) Clone(fileName string) ymlconfig_interf.YmlConfigInterf {
-	//TODO implement me
-	panic("implement me")
+	// 这里存在一个深拷贝，需要注意，避免拷贝的结构体操作对原始结构体造成影响
+	var ymlC = *y
+	var ymlConfViper = *(y.viper)
+	(&ymlC).viper = &ymlConfViper
+
+	(&ymlC).viper.SetConfigName(fileName)
+	if err := (&ymlC).viper.ReadInConfig(); err != nil {
+		variable.ZapLog.Error(my_errors.ErrorsConfigInitFail, zap.Error(err))
+	}
+	return &ymlC
 }
 
 func (y *ymlConfig) Get(keyName string) interface{} {
